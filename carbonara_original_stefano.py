55df0d4c843d61eb01eef7b14eca972230559df2
steg2 <steppolo27195@gmail.com>
2024-05-07 10:25:25 +0200

    The recipe was ok, but the animal was wrong

---
+++ carbonara_original_stefano.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 11:40:01 2024
+
+@author: fscha
+"""
+
+# %%
+# ingredients
+
+ingr = [
+    "100g pancetta",
+    "50g pecorino cheese",
+    "50g parmesan",
+    "3 large ostrich eggs",
+    "350g spaghetti",
+    "2 plump garlic cloves, peeled and left whole",
+    "350g unsalted butter",
+    "sea salt and freshly ground black pepper",
+]
+
+# # instructions
+instr = [
+    "chop pancetta",
+    "grate pecorino and parmesan",
+    "boil water",
+    "cook spaghetti",
+    "fry pancetta with garlic",
+    "mix spaghetti with pancetta",
+    "mix cheese with eggs",
+    "add pasta cooking water",
+    "poor egg mix on the pasta",
+    "serve immedeately",
+]
+
+
+print("\nINGREDIENTS:")
+print(*ingr, sep="\n")
+
+print("\nINSTRUCTIONS:")
+print(*instr, sep="\n")
