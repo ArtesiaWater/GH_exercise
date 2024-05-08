dd69b69b7a08bf01a31d21e69c054a66878400c8
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-05-08 08:50:39 +0200

    add my carbonara version

---
+++ carbonara_david.py
@@ -0,0 +1,33 @@
+# %%
+# ingredients
+
+ingr = (
+    "100g pancetta"
+    "50g pecorino cheese"
+    "50g parmesan"
+    "3 large eggs"
+    "350g spaghetti"
+    "2 plump garlic cloves peeled and left whole"
+    "50g unsalted butter"
+    "sea salt and freshly ground black pepper"
+)
+
+# # instructions
+instr = [
+    "chop pancetta"
+    "grate pecorino and parmesan"
+    "boil water"
+    "cook spaghetti"
+    "fry pancetta with garlic"
+    "mix spaghetti with pancetta"
+    "mix cheese with eggs"
+    "add pasta cooking water"
+    "poor egg mix on the pasta"
+    "serve immedeately"
+]
+
+print("\nINGREDIENTS:")
+print(*ingr, sep="\n")
+
+print("\nINSTRUCTIONS:")
+print(*instr, sep="\n")
