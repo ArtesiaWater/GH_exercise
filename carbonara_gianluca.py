fc4465f25c9da7717e269919c6836876c76b4966
sannachino <gianluca.fiandaca@gmail.com>
2024-05-08 09:42:07 +0200

    Update carbonara_gianluca.py
    Working code and better recipe

--- carbonara_gianluca.py
+++ carbonara_gianluca.py
@@ -27,17 +27,37 @@ instr = [
     'add spagetti to water & salt and turn heat on',
     'cook spaghetti for the time indicated by producer',
     'drain spaghetti, keeping a bit of cooking pasta',
-    'mix spaghetti, yolk and cheese',
-    'add pasta cooking water',
+    'mix spaghetti, yolk and cheese and guanciale',
+    'add pasta cooking water to make a creamy sauce',
     'serve immedeately',
 ]
 
+# # instructions for vegetarians
+veg_instr = [
+    'slice the squash (the best squash is the "napoletana squash" that you find only in Sicily )',
+    'fry the squash in hot olive oil'
+    'mix spaghetti, yolk and cheese and fried squash',
+]
+
+
 
 # Variation: make it vegetarian
-ingr[0] = '100g fried squash'
+ingr_opt = ['300g fried squash']
 
 print('\nINGREDIENTS:')
 print(*ingr, sep='\n')
 
-print('\nINSTRUCTIONS:')
+print('\nOPTIONAL INGREDIENTS FOR VEGETARIAN (NO GUANCIALE):')
+print(*ingr_opt, sep='\n')
+
+print('\nINSTRUCTIONS FOR PISSING YOUR ITALIAN GUESTS OFF:')
 print(*instr, sep='\n')
+
+print('\nINSTRUCTIONS FOR MAKING THE ITALIANS HAPPY:')
+print(*instr[0:4], sep='\n')
+print('add spagetti to boiling water with salt', sep='\n')
+print('cook spaghetti for the time indicated by producer - 2/3 minutes', sep='\n')
+print(*instr[6:10], sep='\n')
+
+print('\nINSTRUCTIONS FOR VEGETARIANS (SUBSTITUTE STEPS 1,2 & 8):')
+print(*veg_instr, sep='\n')
